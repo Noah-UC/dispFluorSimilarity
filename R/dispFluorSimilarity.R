5697d2facc4ca41c05a640cb3e5990baaac93f66
Uddeep <uddeep.c@gmail.com>
2024-11-18 01:25:55 -0700

    Change: Renamed Package to FlowCytoHelp

--- R/dispFluorSimilarity.R
+++ R/dispFluorSimilarity.R
@@ -1,47 +1,51 @@
 
 dispFluorSimilarity <- function(panel) {
 
-require(dplyr)
-require(ggplot2)
+  #installing required packages
 
-cytek_data = read.csv("spectra-11_17_2024, 4_44_54 PM.csv", sep = "," , header = T,  row.names = 1, check.names = FALSE, )
+  usethis::use_package("ggplot2")
+  usethis::use_package("dplyr")
 
-similarity_df =
-  cytek_data %>%
-  filter(
-    row.names(
-      cytek_data) %in% panel) %>%
-  select(panel)
+  #loading database
 
-similarity_matrix = as.matrix(
-    similarity_df
-)
-
-similarity_matrix[upper.tri(similarity_matrix)] =
-  t(
-    similarity_matrix)[upper.tri(similarity_matrix)]
-
-storage.mode(similarity_matrix) <- "numeric"
+  cytek_data = read.csv("../spectra-11_17_2024, 4_44_54 PM.csv", sep = "," , header = T,  row.names = 1, check.names = FALSE, )
 
 
-colnames(simmilarity_matrix) = colnames(simmilarity_df)
+  similarity_df =
+    cytek_data %>%
+    filter(
+      row.names(
+        cytek_data) %in% panel) %>%
+    select(panel)
 
-similarity_long = as.data.frame(
-  as.table(
-    similarity_matrix
-    )
+  similarity_matrix = as.matrix(
+      similarity_df
   )
 
-p <- ggplot(data = similarity_long,
-            aes(x = Var1, y = Var2, fill = Freq)) +
-  geom_tile() +
-  scale_fill_gradient(low = "white", high = "red", name = "Value") +
-  geom_text(aes(label = Freq), color = "black", size = 5) +
-  scale_y_discrete(limits = rev(levels(as.factor(similarity_long$Var2)))) +
-  labs(x = "Fluor A", y = "Fluor B", fill = "Value") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  similarity_matrix[upper.tri(similarity_matrix)] =
+    t(
+      similarity_matrix)[upper.tri(similarity_matrix)]
 
-print(p)
+  storage.mode(similarity_matrix) <- "numeric"
+
+
+
+  similarity_long = as.data.frame(
+    as.table(
+      similarity_matrix
+      )
+    )
+
+  p <- ggplot(data = similarity_long,
+              aes(x = Var1, y = Var2, fill = Freq)) +
+    geom_tile() +
+    scale_fill_gradient(low = "white", high = "red", name = "Value") +
+    geom_text(aes(label = Freq), color = "black", size = 5) +
+    scale_y_discrete(limits = rev(levels(as.factor(similarity_long$Var2)))) +
+    labs(x = "Fluor A", y = "Fluor B", fill = "Value") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+  print(p)
 
 }
